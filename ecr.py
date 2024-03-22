474179c38c1fac4b6085ef68894c269f1090e18e
ROUNAKD007 <110335257+ROUNAKD007@users.noreply.github.com>
2024-03-22 18:00:25 -0500

    Add files via upload

---
+++ ecr.py
@@ -0,0 +1,10 @@
+import boto3
+
+ecr_client = boto3.client('ecr', region_name='us-east-2')
+
+repository_name = "my-cloud_native-repo"
+response = ecr_client.create_repository(repositoryName=repository_name)
+
+
+repository_uri = response['repository']['repositoryUri']
+print(repository_uri)
